8efea5abf808ff8caffe89789b2c044459267843
shubham sapkota <shubham.sapkota1@gmail.com>
2023-03-06 18:32:36 +0545

    user test

--- test/user.test.js
+++ test/user.test.js
@@ -1,175 +1,175 @@
-// let chai = require("chai");
+let chai = require("chai");
 
-// let chaiHttp = require("chai-http");
+let chaiHttp = require("chai-http");
 
-// let server = require("../server");
+let server = require("../server");
 
-// // assertion style
+// assertion style
 
-// chai.should();
+chai.should();
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// describe("florashop User API TEST", () => {
-//   // test the login route
+describe("florashop User API TEST", () => {
+  // test the login route
 
-//   describe("POST /api/login", () => {
-//     it("It should login a user", (done) => {
-//       chai
+  describe("POST /api/login", () => {
+    it("It should login a user", (done) => {
+      chai
 
-//         .request(server)
+        .request(server)
 
-//         .post("/api/v2/login")
+        .post("/api/v2/login")
 
-//         .send({
-//           email: "shubham@gmail.com",
+        .send({
+          email: "shubham@gmail.com",
 
-//           password: "123456789",
-//         })
+          password: "123456789",
+        })
 
-//         .end((err, res) => {
-//           res.should.have.status(201);
+        .end((err, res) => {
+          res.should.have.status(201);
 
-//           done();
-//         });
-//     });
-//   });
-// });
+          done();
+        });
+    });
+  });
+});
 
-// describe("florashop User API TEST", () => {
-//   // test the logout route
+describe("florashop User API TEST", () => {
+  // test the logout route
 
-//   describe("GET /api/logout", () => {
-//     it("It should logout a user", (done) => {
-//       chai
+  describe("GET /api/logout", () => {
+    it("It should logout a user", (done) => {
+      chai
 
-//         .request(server)
+        .request(server)
 
-//         .get("/api/v2/logout")
+        .get("/api/v2/logout")
 
-//         .end((err, res) => {
-//           res.should.have.status(200);
+        .end((err, res) => {
+          res.should.have.status(200);
 
-//           done();
-//         });
-//     });
-//   });
-// });
+          done();
+        });
+    });
+  });
+});
 
-// describe("florashop User API TEST", () => {
-//   // test the userdetails route
+describe("florashop User API TEST", () => {
+  // test the userdetails route
 
-//   describe("GET /api/userdetails", () => {
-//     it("It should get a user details", (done) => {
-//       chai
+  describe("GET /api/userdetails", () => {
+    it("It should get a user details", (done) => {
+      chai
 
-//         .request(server)
+        .request(server)
 
-//         .get("/api/v2/me")
+        .get("/api/v2/me")
 
-//         .end((err, res) => {
-//           res.should.have.status(401);
+        .end((err, res) => {
+          res.should.have.status(401);
 
-//           done();
-//         });
-//     });
-//   });
-// });
+          done();
+        });
+    });
+  });
+});
 
-// describe("florashop User API TEST", () => {
-//   // test the updatePassword route
+describe("florashop User API TEST", () => {
+  // test the updatePassword route
 
-//   describe("PUT /api/updatePassword", () => {
-//     it("It should update a user password", (done) => {
-//       chai
+  describe("PUT /api/updatePassword", () => {
+    it("It should update a user password", (done) => {
+      chai
 
-//         .request(server)
+        .request(server)
 
-//         .put("/api/v2/me/update")
+        .put("/api/v2/me/update")
 
-//         .send({
-//           password: "test123",
+        .send({
+          password: "test123",
 
-//           newPassword: "test123",
+          newPassword: "test123",
 
-//           passwordConfirm: "test123",
-//         })
+          passwordConfirm: "test123",
+        })
 
-//         .end((err, res) => {
-//           res.should.have.status(401);
+        .end((err, res) => {
+          res.should.have.status(401);
 
-//           done();
-//         });
-//     });
-//   });
-// });
-
-
+          done();
+        });
+    });
+  });
+});
 
 
-// describe("florashop User API TEST", () => {
-//   // test the delete route
 
-//   describe("GET /api/user deleted", () => {
-//     it("It should get a user deleted", (done) => {
-//       chai
 
-//         .request(server)
+describe("florashop User API TEST", () => {
+  // test the delete route
 
-//         .delete("/api/v2/admin/user/:id")
+  describe("GET /api/user deleted", () => {
+    it("It should get a user deleted", (done) => {
+      chai
 
-//         .end((err, res) => {
-//           res.should.have.status(401);
+        .request(server)
 
-//           done();
-//         });
-//     });
-//   });
-// });
+        .delete("/api/v2/admin/user/:id")
 
-// describe("florashop User API TEST", () => {
-//   // test the delete route
+        .end((err, res) => {
+          res.should.have.status(401);
 
-//   describe("GET /api/user updated", () => {
-//     it("It should get a user updated", (done) => {
-//       chai
+          done();
+        });
+    });
+  });
+});
 
-//         .request(server)
+describe("florashop User API TEST", () => {
+  // test the delete route
 
-//         .put("/api/v2/admin/user/:id")
+  describe("GET /api/user updated", () => {
+    it("It should get a user updated", (done) => {
+      chai
 
-//         .end((err, res) => {
-//           res.should.have.status(401);
+        .request(server)
 
-//           done();
-//         });
-//     });
-//   });
-// });
-// describe("florashop User API TEST", () => {
-//   // test the login route
+        .put("/api/v2/admin/user/:id")
 
-//   describe("POST /api/register", () => {
-//     it("It should register a user", (done) => {
-//       chai
+        .end((err, res) => {
+          res.should.have.status(401);
 
-//         .request(server)
+          done();
+        });
+    });
+  });
+});
+describe("florashop User API TEST", () => {
+  // test the login route
 
-//         .post("/api/v2/register")
+  describe("POST /api/register", () => {
+    it("It should register a user", (done) => {
+      chai
 
-//         .send({
-//           name:"mohansapkota",
-//           email: "mohan11@gmail.com",
+        .request(server)
 
-//           password: "123456789",
-//           image:"https://example.com/profile.jpg",
-//         })
+        .post("/api/v2/register")
 
-//         .end((err, res) => {
-//           res.should.have.status(201);
+        .send({
+          name:"mohansapkota",
+          email: "mohan11@gmail.com",
 
-//           done();
-//         });
-//     });
-//   });
-// });
+          password: "123456789",
+          image:"https://example.com/profile.jpg",
+        })
+
+        .end((err, res) => {
+          res.should.have.status(201);
+
+          done();
+        });
+    });
+  });
+});
