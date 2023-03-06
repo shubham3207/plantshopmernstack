0f507e28945309fe7bb3dafd3b78913fcf046fa8
shubham sapkota <shubham.sapkota1@gmail.com>
2023-03-06 18:32:05 +0545

    product test

--- test/product.test.js
+++ test/product.test.js
@@ -1,77 +1,77 @@
-// let chai = require("chai");
+let chai = require("chai");
 
-// let chaiHttp = require("chai-http");
+let chaiHttp = require("chai-http");
 
-// let server = require("../server");
+let server = require("../server");
 
-// // assertion style
+// assertion style
 
-// chai.should();
+chai.should();
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// describe("florashop User API TEST", () => {
-//  // test the product route
+describe("florashop User API TEST", () => {
+ // test the product route
  
-//   describe("POST /api/products", () => {
-//     it("It should get products", (done) => {
-//       chai
+  describe("POST /api/products", () => {
+    it("It should get products", (done) => {
+      chai
 
-//         .request(server)
+        .request(server)
 
-//         .get("/api/v2/products")
+        .get("/api/v2/products")
 
-//         .end((err, res) => {
-//           res.should.have.status(200);
+        .end((err, res) => {
+          res.should.have.status(200);
 
-//           done();
-//         });
-//     });
-//   });
-// });
-
-
+          done();
+        });
+    });
+  });
+});
 
 
 
-// describe("florashop review API TEST", () => {
-//  // test the product route
+
+
+describe("florashop review API TEST", () => {
+ // test the product route
  
-//   describe("POST /api/reviews", () => {
-//     it("It should get products review", (done) => {
-//       chai
+  describe("POST /api/reviews", () => {
+    it("It should get products review", (done) => {
+      chai
 
-//         .request(server)
+        .request(server)
 
-//         .get("/api/v2/reviews")
+        .get("/api/v2/reviews")
 
-//         .end((err, res) => {
-//           res.should.have.status(404);
+        .end((err, res) => {
+          res.should.have.status(404);
 
-//           done();
-//         });
-//     });
-//   });
-// });
-// describe("florashop review API TEST", () => {
-//   // test the product route
+          done();
+        });
+    });
+  });
+});
+describe("florashop review API TEST", () => {
+  // test the product route
   
-//    describe("POST /api/reviews", () => {
-//      it("It should get products review", (done) => {
-//        chai
+   describe("POST /api/reviews", () => {
+     it("It should get products review", (done) => {
+       chai
  
-//          .request(server)
+         .request(server)
  
-//          .delete("/api/v2/admin/reviews")
+         .delete("/api/v2/admin/reviews")
  
-//          .end((err, res) => {
-//            res.should.have.status(404);
+         .end((err, res) => {
+           res.should.have.status(404);
  
-//            done();
-//          });
-//      });
-//    });
-//  });
+           done();
+         });
+     });
+   });
+ });
  
 
 
